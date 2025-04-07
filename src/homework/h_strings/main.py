fe2cb14bd6938a4b63c39b68bcee704bffc4c159
maranders21 <marissa.anderson@g.austincc.edu>
2025-04-07 21:36:06 +0000

    Homework 4 maranders21

--- src/homework/h_strings/main.py
+++ src/homework/h_strings/main.py
@@ -1,1 +1,28 @@
 #
+from strings import get_hamming_distance
+from strings import get_dna_complement
+
+def main():
+    while True:
+        print("1-Hamming Distance")
+        print("2-Dna Complement")
+        print("3-Exit")
+        user_choice = int(input("Choose a menu option: "))
+        if user_choice == 1:
+            dna1 = input("First DNA string: ")
+            dna2 = input("Second DNA string: ")
+            if len(dna1) != len(dna2):
+                print("Error - DNA strings must be the same length to get a hamming distance.")
+            else:
+                dist_result = get_hamming_distance(dna1, dna2)
+                print(f"The hamming distance of the 2 strings is {dist_result}.")
+        elif user_choice == 2:
+            dna1 = input("Original DNA string: ")
+            complement_result = get_dna_complement(dna1)
+            print("The DNA reverse complement of ", dna1, " is ", complement_result, ".")
+        elif user_choice == 3:
+            break
+        else:
+            print("Error! Invalid Menu Choice!")
+
+main()
--- src/homework/h_strings/strings.py
+++ src/homework/h_strings/strings.py
@@ -1,1 +1,22 @@
 #
+def get_hamming_distance(dna1, dna2):
+    hamming_distance = 0
+    for i in range(len(dna1)):
+        if dna1[i] != dna2[i]:
+            hamming_distance += 1
+    return hamming_distance
+
+def get_dna_complement(dna):
+    reverse_complement = ""
+    char_com = "A"
+    for char in dna:
+        if char == "A":
+            char_com = "T"
+        elif char == "T":
+            char_com = "A"
+        elif char == "C":
+            char_com = "G"
+        elif char == "G":
+            char_com = "C"    
+        reverse_complement = char_com + reverse_complement
+    return reverse_complement
