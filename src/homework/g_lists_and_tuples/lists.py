51380c5b6944782c4b1eb3dd0b10a5cfceb4c73e
maranders21 <marissa.anderson@g.austincc.edu>
2025-04-07 21:57:03 +0000

    Homework 5 maranders21

--- src/homework/g_lists_and_tuples/lists.py
+++ src/homework/g_lists_and_tuples/lists.py
@@ -1,1 +1,14 @@
 #
+def get_lowest_list_value(list):
+    min_value = list[0]
+    for item in list:
+        if item < min_value:
+            min_value = item
+    return min_value
+
+def get_highest_list_value(list):
+    max_value = list[0]
+    for item in list:
+        if item > max_value:
+            max_value = item
+    return max_value
--- src/homework/g_lists_and_tuples/main.py
+++ src/homework/g_lists_and_tuples/main.py
@@ -1,1 +1,31 @@
 #
+from lists import get_lowest_list_value
+from lists import get_highest_list_value
+
+def main():
+    while True:
+        print("1-Show the list low/high values")
+        print("2-Exit")
+        user_choice = int(input("Choose option: "))
+        if user_choice == 1:
+            input_list = []
+            count = 0
+            cont_choice = True
+            while count < 3 or cont_choice == True:
+                temp_val = int(input("Enter a list value: "))
+                input_list.append(temp_val)
+                count += 1
+                if count > 2:
+                    cont_val = str(input("Do you want to enter another value? Y/N... "))
+                    if cont_val.lower() == "n":
+                        cont_choice = False
+            min_val = get_lowest_list_value(input_list)
+            max_val = get_highest_list_value(input_list)
+            print(f"The minimum list value is {min_val}.")
+            print(f"The maximum list value is {max_val}.")
+        elif user_choice == 2:
+            break
+        else:
+            print("Error! Invalid choice!")
+
+main()
