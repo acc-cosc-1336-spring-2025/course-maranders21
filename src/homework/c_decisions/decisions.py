67edab0bec0e9c0c46ef94e75d4900f10818315c
maranders21 <marissa.anderson@g.austincc.edu>
2025-04-07 15:25:42 +0000

    Homework 2 maranders21

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -1,1 +1,18 @@
 #
+def get_options_ratio(option, total):
+    ratio = option / total
+    return ratio
+
+def get_faculty_rating(ratio):
+    if ratio >= 0.9 and ratio < 1:
+        return "Excellent"
+    elif ratio > 0.8 and ratio < 1:
+        return "Very Good"
+    elif ratio > 0.7 and ratio < 1:
+        return "Good"
+    elif ratio > 0.6 and ratio < 1:
+        return "Needs Improvement"
+    elif ratio >= 0 and ratio < 1:
+        return "Unacceptable"
+    else:
+        return "Error"
--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,1 +1,11 @@
 #
+from decisions import get_options_ratio
+from decisions import get_faculty_rating
+
+def main():
+    user_options = float(input("Please enter options: "))
+    user_total = float(input("Please enter total: "))
+    result = get_options_ratio(user_options, user_total)
+    print (get_faculty_rating(result))
+                       
+main()
