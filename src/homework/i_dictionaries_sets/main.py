090b2d89133801c3fee78207e3d0ca9e217da14f
maranders21 <marissa.anderson@g.austincc.edu>
2025-04-14 20:54:24 +0000

    maranders21 homework 7

--- src/homework/i_dictionaries_sets/main.py
+++ src/homework/i_dictionaries_sets/main.py
@@ -1,1 +1,27 @@
 #
+from dictionary import add_inventory
+from dictionary import remove_inventory_widget
+main_dictionary = {}
+
+def main():
+    while True:
+        print("Inventory Menu")
+        print("1- Add or Update Item")
+        print("2-Delete Item")
+        print("3-Exit")
+        user_choice = int(input("Choose Menu Option: "))
+        if user_choice == 1:
+            insert_key = input("What is the key for the dictionary item you wish to add? ")
+            insert_value = input("What is the value you wish to assign / add to this key? ")
+            add_inventory(main_dictionary, insert_key, insert_value)
+            print("Inserted!")
+        elif user_choice == 2:
+            delete_key = input("What is the key for the dictionary item you wish to remove? ")
+            print(remove_inventory_widget(main_dictionary(delete_key)))
+        elif user_choice == 3:
+            break
+        else:
+            print("Error! Invalid Menu Option...")
+
+main()
+
--- tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
+++ tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -1,1 +1,33 @@
 #
+import unittest
+#src/homework/i_dictionaries_sets/dictionary.py
+from src.homework.i_dictionaries_sets.dictionary import add_inventory
+from src.homework.i_dictionaries_sets.dictionary import remove_inventory_widget
+inventory_dictionary = {}
+
+
+class Test_Config(unittest.TestCase):
+    def test_add_inventory_1(self):
+        #test that the function returns 10 under key Widget1
+        add_inventory(inventory_dictionary, "Widget1", 10)
+        self.assertEqual(10, inventory_dictionary["Widget1"])
+
+    def test_add_inventory_2(self):
+        #test that the function returns 35 under key Widget1
+        add_inventory(inventory_dictionary, "Widget1", 25)
+        self.assertEqual(35, inventory_dictionary["Widget1"])
+
+    def test_add_inventory_3(self):
+        #test that the function returns 35 under key Widget1
+        add_inventory(inventory_dictionary, "Widget1", -10)
+        self.assertEqual(25, inventory_dictionary["Widget1"])
+
+    def test_remove_inventory_widget_1(self):
+        #test that the function returns a dictionary length of 1
+        #test that the function returns True - if widget2 exists
+        test_dictionary = {}
+        add_inventory(test_dictionary, "widget1", 1)
+        add_inventory(test_dictionary, "widget2", 2)
+        self.assertEqual("Record deleted", remove_inventory_widget(test_dictionary, "widget1"))
+        self.assertEqual(1, len(test_dictionary))
+        self.assertEqual(True, "widget2" in test_dictionary)
