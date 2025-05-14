21114c2081ece8275ed0365436003add7305f876
maranders21 <marissa.anderson@g.austincc.edu>
2025-05-14 23:47:01 +0000

    Homework 8

--- src/homework/j_classes/main.py
+++ src/homework/j_classes/main.py
@@ -1,1 +1,19 @@
 #
+import class_a
+
+def main():
+    user_ext = False
+    while user_ext == False:
+        Hmwk_Die = class_a.Die()
+        Hmwk_Die.roll()
+        user_input = input("Do you want to continue? Y/N ")    
+        if user_input.upper() == "N":
+            return
+        print(Hmwk_Die)
+    
+
+
+
+
+
+main()
--- tests/homework/j_classes/tests_classes.py
+++ tests/homework/j_classes/tests_classes.py
@@ -1,1 +1,29 @@
 #
+import unittest
+from src.homework.j_classes import class_a
+
+class Test_Config(unittest.TestCase):
+    def test_Die_1(self):
+        test_1 = class_a.Die()
+        test_1.roll()
+        test_result_1 = False
+        if test_1.get_rolled_value() >= 1 and test_1.get_rolled_value() <= 6:
+            test_result_1=True
+        self.assertEqual(True, test_result_1)
+        
+    def test_Die_2(self):
+        test_2 = class_a.Die()
+        test_2.roll()
+        test_result_2 = False
+        if test_2.get_rolled_value() >= 1 and test_2.get_rolled_value() <= 6:
+            test_result_2=True
+        self.assertEqual(True, test_result_2)
+    
+    def test_Die_3(self):
+        test_3 = class_a.Die()
+        test_3.roll()
+        test_result_3 = False
+        if test_3.get_rolled_value() >= 1 and test_3.get_rolled_value() <= 6:
+            test_result_3=True
+        self.assertEqual(True, test_result_3)
+        
